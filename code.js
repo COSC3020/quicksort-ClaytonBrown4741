47ac131ac9b8d19180b8ff51e37a5ff68df9357b
Clayton Brown <111392580+ClaytonBrown4741@users.noreply.github.com>
2023-10-23 10:52:02 -0600

    Made quicksort in place

--- code.js
+++ code.js
@@ -8,47 +8,53 @@ function quicksort(arr)
 
 function quickSortIterative(arr, lo, hi)
 {
-    var stack = new Array(hi);
-    stack.fill(0);
-    var topOfStack=0;
+    //var stack = new Array(hi);
+    //stack.fill(0);
+    //var topOfStack=0;
     var bufferVariable=-1;
     var pivot=-1;
-    stack[topOfStack]=lo;
-    topOfStack++;
-    stack[topOfStack]=hi;
+    //stack[topOfStack]=lo;
+    //topOfStack++;
+    //stack[topOfStack]=hi;
+    
+    var index=0;
 
-    while (topOfStack > -1){
-	    hi=stack[topOfStack];
-	    topOfStack--;
-	    lo=stack[topOfStack];
-	    topOfStack--;
-
-	    pivot = lo;
-       	    for(i = lo + 1; i <= hi; i++){
-               	    if(arr[i] < arr[lo]){
-                       	    bufferVariable=arr[pivot+1];
-                       	    arr[pivot+1]=arr[i];
-                       	    arr[i]=bufferVariable;
-                       	    pivot++;
-               	    }
-       	    }
-       	    bufferVariable=arr[lo];
-       	    arr[lo]=arr[pivot];
-      	    arr[pivot]=bufferVariable;
-	    
-	    
-	    if (pivot-1 > lo){
-		    topOfStack++;
-		    stack[topOfStack]=lo;
-		    topOfStack++;
-		    stack[topOfStack]=pivot-1;
+    while (index < hi){
+	//    hi=stack[topOfStack];
+	 //   topOfStack--;
+	 //   lo=stack[topOfStack];
+	 //   topOfStack--;
+	    if (arr[index] > arr[index+1]){
+	    	pivot = index;
+       	    	for(i = lo + 1; i <= hi; i++){
+               		    if(arr[i] < arr[lo]){
+                       		    bufferVariable=arr[pivot+1];
+                       		    arr[pivot+1]=arr[i];
+                       	   	 arr[i]=bufferVariable;
+                       	   	 pivot++;
+               	   	 }
+       	   	 }
+       	    	bufferVariable=arr[lo];
+       	   	 arr[lo]=arr[pivot];
+      	   	 arr[pivot]=bufferVariable;
+		 index=0;
 	    }
-	    if (pivot+1 < hi){
-		    topOfStack++;
-		    stack[topOfStack]=pivot+1;
-		    topOfStack++;
-		    stack[topOfStack]=hi;
-	    }
+	    else
+		    index++;
+	   // if (pivot-1 > lo){
+		//    topOfStack++;
+		 //   stack[topOfStack]=lo;
+		  //  topOfStack++;
+		  //  stack[topOfStack]=pivot-1;
+	   // }
+	   // if (pivot+1 < hi){
+	//	    topOfStack++;
+	//	    stack[topOfStack]=pivot+1;
+	//	    topOfStack++;
+	//	    stack[topOfStack]=hi;
+	 //   }
     }
     return arr;
 }
+
+//console.log(quicksort([3,5,4,3,2,5,10,1]))
